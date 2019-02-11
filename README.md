e5d531016069bb71c1d14e7dbd9e948f91bf391c
j3rome <jerome.abdelnour@usherbrooke.ca>
2019-02-11 17:22:36 -0500

    + First version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# arXivDD
-arXiv Direct Download Chrome Extension
+# arXivDDL
+arXiv is a great resource but downloading article directly from the website inevitably result in this:
+![alt text](theProblem.png)
+
+I got tired of it and I quickly hacked a TamperMonkey script to inject a new download button on arXiv. I am simply using the `download` html attribute to propose a sensible name for the PDF file (the article title)
+
+![alt text](result.png)
+I thought others might find it usefull so I bundled it in this small chrome extension.
+
+Feel free enhance this as you wish !
+
---
+++ addDownloadLink.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    // Create a new link beside the original download link
+    // Append the "Download" html attribute to the link with full article name
+    var downloadLinkList = document.querySelector("div#abs div.extra-services ul li:first-child");
+    var browserLink = downloadLinkList.children[0];
+    // TODO : Check for encoding issues/unpermitted chars? Browser and OS can probably take care of this..
+    var filename = document.title.replace("] ","]").replace(/ /g,"_") + ".pdf";
+
+    var directLink = browserLink.cloneNode();
+    directLink.innerText = "[Direct Download]";
+    directLink.setAttribute("download",filename);
+    directLink.setAttribute("style","margin-left:10px");
+
+    downloadLinkList.append(directLink);
+
+})();
