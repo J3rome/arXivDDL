3003d3766da66bccdd167af16827419f6028efd3
j3rome <jerome.abdelnour@usherbrooke.ca>
2021-08-01 05:05:34 -0400

    * Updated dom selector

--- addDownloadLink.js
+++ addDownloadLink.js
@@ -3,7 +3,7 @@
 
     // Create a new link beside the original download link
     // Append the "Download" html attribute to the link with full article name
-    var downloadLinkList = document.querySelector("div#abs div.extra-services ul li:first-child");
+    var downloadLinkList = document.querySelector("div div.extra-services ul li:first-child");
     var browserLink = downloadLinkList.children[0];
     // TODO : Check for encoding issues/unpermitted chars? Browser and OS can probably take care of this..
     var filename = document.title.replace("] ","]").replace(/ /g,"_") + ".pdf";
